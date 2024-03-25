22db03f72b36c108b5494611d2abc84f51954c05
Thang Duong <thangduong12322@gmail.com>
2024-03-25 15:52:47 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import "./App.css";
+import { Experience } from "./components/Experience";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="profile">
+        <h1>
+          Duong Huu Thang
+        </h1>
+      </div>
+      <div className="Experience">
+        <Canvas
+          camera={{
+            fov: 50,
+            position: [5, 2, 2.5],
+          }}
+        >
+          <Experience />
+        </Canvas>
+      </div>
+    </div>
+  );
+}
+useGLTF.preload("./models/WawaOffice.glb");
+
+export default App;
---
+++ src/components/Experience.tsx
@@ -0,0 +1,17 @@
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+export const Experience = () => {
+  const { nodes, materials } = useGLTF("./models/WawaOffice.glb");
+
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <OrbitControls enableZoom={false} />
+      {/* <ScrollControls damping={0.25}> */}
+      {/* <Overlay /> */}
+      <mesh geometry={nodes["01_office"].geometry} material={materials["01"]} />
+      {/* <Office /> */}
+      {/* </ScrollControls> */}
+    </>
+  );
+};
